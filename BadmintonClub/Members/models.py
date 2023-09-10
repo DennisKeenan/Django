1fc1665ca45969096493e7b15e6b4a817a687e3f
DennisKeenan <dennis.k.arkananta@gmail.com>
2023-09-10 11:28:55 +0700

    Create Database and Details

--- BadmintonClub/Members/models.py
+++ BadmintonClub/Members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    First_Name=models.CharField(max_length=15)
+    Last_Name=models.CharField(max_length=30)
+    Phone_Number=models.IntegerField(null=True)
+    Join=models.DateField(null=True)
--- BadmintonClub/Members/views.py
+++ BadmintonClub/Members/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Member
 # Create your views here.
 def members(request):
+    MyMembers=Member.objects.all().values()
     template=loader.get_template('index.html')
-    return HttpResponse(template.render())
+    Context={
+        'MyMembers':MyMembers
+    }
+    return HttpResponse(template.render(Context,request))
+def details(request,id):
+    MyMember=Member.objects.get(id=id)
+    template=loader.get_template('detail.html')
+    Context={
+        'MyMember':MyMember
+    }
+    return HttpResponse(template.render(Context,request))
